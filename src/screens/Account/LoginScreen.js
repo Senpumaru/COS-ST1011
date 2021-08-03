8f4932fb0b9e815b89e338acdfb68240d30ddacb
Senpumaru <hakumen737@gmail.com>
2021-08-03 12:06:09 +0300

    Test: React Issue

--- src/screens/Account/LoginScreen.js
+++ src/screens/Account/LoginScreen.js
@@ -1,158 +1,163 @@
-import Avatar from '@material-ui/core/Avatar';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
-import Container from '@material-ui/core/Container';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../../actions/Account/UserActions';
-import Loader from '../../components/Loader';
-import Message from '../../components/Message';
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import Container from "@material-ui/core/Container";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../actions/Account/UserActions";
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="#">
-                ACS
-      </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        ACS
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        color: "secondary",
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    color: "secondary",
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function LoginScreen({ location, history }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [remember, setRemember] = useState(true)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(true);
 
-    const dispatch = useDispatch()
-    const redirect = location.search ? location.search.split("=")[1] : "/"
+  const dispatch = useDispatch();
+  const redirect = location.search ? location.search.split("=")[1] : "/";
 
-    const userLogin = useSelector(state => state.Profile["userLogin"])
-    const { loading, error, userInfo } = userLogin
+  const userLogin = useSelector((state) => state.Profile["userLogin"]);
+  const { loading, error, userInfo } = userLogin;
 
-    useEffect(() => {
-        if (userInfo) {
-            history.push(redirect)
-        }
-    }, [history, userInfo, redirect])
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        dispatch(login(email, password, remember))
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
     }
+  }, [history, userInfo, redirect]);
 
-    // Remember me
-    const handleChange = (event) => {
-        setRemember(event.target.checked);
-    };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password, remember));
+  };
 
-    return (
+  // Remember me
+  const handleChange = (event) => {
+    setRemember(event.target.checked);
+  };
 
-        <Container component="main" maxWidth="xs">
-            {error && <Message severity="error">{error}</Message>}
-            
+  return (
+    <Container component="main" maxWidth="xs">
+      {error && <Message severity="error">{error}</Message>}
 
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                Вход в приложение ИГХ: PD-L1
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <h1>Test: process.env.REACT_APP_DEBUG</h1>
+        <h1>Test: process.env.REACT_APP_HOST_IP_ADDRESS</h1>
+        <h1>Test: process.env.REACT_APP_NAME</h1>
+        <Typography component="h1" variant="h5">
+          Вход в  ИГХ: PD-L1
         </Typography>
-                <form className={classes.form} onSubmit={submitHandler}>
-                    <TextField
-                        variant="outlined"
-                        color="primary"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        value={email}
-                        label="e-mail"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        color="primary"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Пароль"
-                        type="password"
-                        id="password"
-                        value={password}
-                        autoComplete="current-password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <FormControlLabel
-                        label="Запомнить меня"
-                        control={
-                            <Checkbox
-                                checked={remember}
-                                onChange={handleChange}
-                                color="secondary" />
-                        }
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Авторизация
-                    </Button>
-                    {loading && <Loader />}
-                    <Grid container justify="flex-end">
-
-                        <Grid item >
-                            <Link href={redirect ? `/register?redirect=${redirect}` : "register"} color="primary" variant="body2">
-                                Нет акканута? Пройдите регистрацию
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-        </Container>
-    );
+        <form className={classes.form} onSubmit={submitHandler}>
+          <TextField
+            variant="outlined"
+            color="primary"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            value={email}
+            label="e-mail"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            color="primary"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Пароль"
+            type="password"
+            id="password"
+            value={password}
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            label="Запомнить меня"
+            control={
+              <Checkbox
+                checked={remember}
+                onChange={handleChange}
+                color="secondary"
+              />
+            }
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Авторизация
+          </Button>
+          {loading && <Loader />}
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link
+                href={redirect ? `/register?redirect=${redirect}` : "register"}
+                color="primary"
+                variant="body2"
+              >
+                Нет акканута? Пройдите регистрацию
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
